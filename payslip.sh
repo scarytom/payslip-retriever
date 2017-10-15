33d27c96bf72db52ad51f7fa97c8ba1df2edc5ca
Tom Denley <t.denley@cantab.net>
2017-10-15 23:18:02 +0100

    allow payslip date to be specified

--- payslip.sh
+++ payslip.sh
@@ -18,6 +18,11 @@ usage() {
 	echo "  -e <employee code>, --employee-code <employee code>"
 	echo "      employee code for payroll system (e.g. 1234567)"
 	echo
+	echo "  -d <date>, --payslip-date <date>"
+	echo "      date of the payslip"
+	echo "      if you specify 'yyyy-mm' only, the day will be calculated best effort"
+	echo "      if unspecified, will use retrieve payslip for the current month"
+	echo
 	echo "  -p <file>, --password-file <file>"
 	echo "      file containing password. if unspecified, program will prompt for password"
 	echo
@@ -31,6 +36,7 @@ fail() {
         exit 1
 }
 
+PAYSLIP_DATE="$(date '+%Y-%m')"
 PASSWORD_FILE='/dev/null'
 OUT_FILE='payslip.pdf'
 
@@ -47,6 +53,10 @@ do
 		;;
 	-e|--employee-code)
 		EMPLOYEE_CODE="${2}"
+                shift
+		;;
+	-d|--payslip-date)
+		PAYSLIP_DATE="${2}"
                 shift
 		;;
 	-p|--password-file)
@@ -73,11 +83,58 @@ else
   PASSWORD_ARG='--ask-password'
 fi
 
-PAY_RUN_DATE='20170927'   # for August, set to '20170825'
-PAY_RUN_CODE="${PAY_RUN_DATE}0001"
-PAY_RUN_ENTRY_CODE='20170006' # this is tax year offset (Apr2017 is '20170001' and Mar2018 is '20170012' I guess)
+datefunc() {
+        # TODO: detect OSX and then use gdate from coreutils (or use -v?)
+        date -d "${1}" "${2}"
+}
+
+calc_payroll_date() {
+        # first weekday on-or-before 27th of the month
+        YEARMONTH="$(datefunc "${1}" '+%Y-%m')"
+        TWENTYSEVENTH="${YEARMONTH}-27"
+        case "$(datefunc "${TWENTYSEVENTH}" '+%a')" in
+            Sat)
+                echo "${YEARMONTH}-26"
+                ;;
+            Sun)
+                echo "${YEARMONTH}-25"
+                ;;
+            *)
+                echo "${TWENTYSEVENTH}"
+                ;;
+        esac
+}
+
+calc_run_entry_code() {
+        # this is tax year offset (Apr2017 is '20170001' and Mar2018 is '20170012' I guess)
+        MONTH="$(datefunc "${1}" '+%-m')"
+        YEAR="$(datefunc "${1}" '+%Y')"
+
+        if [ "${MONTH}" -lt '4' ]; then
+            echo "$((${YEAR} - 1))00$((${MONTH} + 9))"
+        else
+            echo "${YEAR}000$((${MONTH} - 3))"
+        fi
+}
+
+if [ "$(echo -n "${PAYSLIP_DATE}" | wc -c)" -eq 7 ]; then
+    PAYSLIP_DATE="$(calc_payroll_date "${PAYSLIP_DATE}-01")"
+fi
+
+PAY_RUN_CODE="$(datefunc "${PAYSLIP_DATE}" '+%Y%m%d')0001"
+PAY_RUN_ENTRY_CODE="$(calc_run_entry_code "${PAYSLIP_DATE}")"
 EE_PAYROLL_CODE='001'
 EE_SEPARATE_CHECK='0'
+JURISDICTION='UK'
+FUNCTION='EPayslip' # for P60 set to 'EP60'
+E_DATE='20070101'   # for P60 set to '20160406' say
+YEAR='2007'         # for P60 set to '2016' say
+QUARTER='1'         # for P60 set to '2'
+MONTH='1'           # for P60 set to '4'
+
+# need to find out what happens to these (for P60)
+#TaxOffice=384
+#PayeRef=XX12345
 
 wget -q --user="${USERNAME}" "${PASSWORD_ARG}" \
   --save-cookies 'cookies.txt' \
@@ -89,17 +146,6 @@ TOKEN="$(wget -q -O - \
   --load-cookies 'cookies.txt' \
   'https://fress2.adp.com/core/coreControl.asp?ProductType=0' \
   | grep sessionToken | cut -d "'" -f2)"
-
-JURISDICTION='UK'
-FUNCTION='EPayslip' # for P60 set to 'EP60'
-E_DATE='20070101'   # for P60 set to '20160406' say
-YEAR='2007'         # for P60 set to '2016' say
-QUARTER='1'         # for P60 set to '2'
-MONTH='1'           # for P60 set to '4'
-
-# need to find out what happens to these (for P60)
-#TaxOffice=384
-#PayeRef=XX12345
 
 wget -q \
   --load-cookies 'cookies.txt' \
